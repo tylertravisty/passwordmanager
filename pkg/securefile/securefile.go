8da26725adb767410b1471fda49b4d2cd8e81ed1
tyler <tylertravisty@users.noreply.github.com>
2021-05-22 22:20:42 -0400

    Added table tests to securefile package.

--- pkg/securefile/securefile.go
+++ pkg/securefile/securefile.go
@@ -53,7 +53,7 @@ type ReadSaver struct {
 func (rs *ReadSaver) Read() ([]byte, error) {
 	fileBytes, err := ioutil.ReadFile(rs.Filepath)
 	if err != nil {
-		return nil, fmt.Errorf("Could not read file %s: %v", rs.Filepath, err)
+		return nil, fmt.Errorf("securefile: Could not read file %s: %v", rs.Filepath, err)
 	}
 
 	magic := magicNumber()
@@ -61,33 +61,33 @@ func (rs *ReadSaver) Read() ([]byte, error) {
 	fileSignature, salt, bcryptHash, nonce, ciphertext := fileBytes[:len(magic)], fileBytes[len(magic):len(magic)+keySaltSize], fileBytes[len(magic)+keySaltSize:len(magic)+keySaltSize+bcryptSize], fileBytes[len(magic)+keySaltSize+bcryptSize:len(magic)+keySaltSize+bcryptSize+gcmNonceSize], fileBytes[len(magic)+keySaltSize+bcryptSize+gcmNonceSize:]
 
 	if !bytes.Equal(fileSignature, magic) {
-		return nil, fmt.Errorf("File signature does not match magic number")
+		return nil, fmt.Errorf("securefile: File signature does not match magic number")
 	}
 
 	key := pbkdf2.Key([]byte(rs.Password), salt, keyIter, keyLength, sha256.New)
 
 	err = bcrypt.CompareHashAndPassword(bcryptHash, key)
 	if err != nil {
-		return nil, fmt.Errorf("Could not verify password is valid: %v", err)
+		return nil, fmt.Errorf("securefile: Could not verify password is valid: %v", err)
 	}
 
 	c, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, fmt.Errorf("Error while creating new AES cipher: %v", err)
+		return nil, fmt.Errorf("securefile: Error while creating new AES cipher: %v", err)
 	}
 
 	gcm, err := cipher.NewGCMWithNonceSize(c, gcmNonceSize)
 	if err != nil {
-		return nil, fmt.Errorf("Error while creating new GCM: %v", err)
+		return nil, fmt.Errorf("securefile: Error while creating new GCM: %v", err)
 	}
 
 	if len(ciphertext) < gcm.NonceSize() {
-		return nil, fmt.Errorf("Ciphertext is smaller than GCM nonce size")
+		return nil, fmt.Errorf("securefile: Ciphertext is smaller than GCM nonce size")
 	}
 
 	fulltext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
-		return nil, fmt.Errorf("Error while opening ciphertext: %v", err)
+		return nil, fmt.Errorf("securefile: Error while opening ciphertext: %v", err)
 	}
 
 	plaintext := removePrefix(fulltext)
@@ -99,38 +99,38 @@ func (rs *ReadSaver) Read() ([]byte, error) {
 func (rs *ReadSaver) Save(plaintext []byte) error {
 	fulltext, err := addPrefix(plaintext)
 	if err != nil {
-		return fmt.Errorf("Could not add byte prefix to plaintext: %v", err)
+		return fmt.Errorf("securefile: Could not add byte prefix to plaintext: %v", err)
 	}
 
 	salt, err := libpassword.Random(keySaltSize)
 	if err != nil {
-		return fmt.Errorf("Error while generating salt for encryption key: %v", err)
+		return fmt.Errorf("securefile: Error while generating salt for encryption key: %v", err)
 	}
 
 	key := pbkdf2.Key([]byte(rs.Password), salt, keyIter, keyLength, sha256.New)
 
 	bcryptHash, err := bcrypt.GenerateFromPassword(key, bcryptCost)
 	if len(bcryptHash) != bcryptSize {
-		return fmt.Errorf("Length of generated bcrypt hash %d does not equal expected bcrypt hash size %d", len(bcryptHash), bcryptSize)
+		return fmt.Errorf("securefile: Length of generated bcrypt hash %d does not equal expected bcrypt hash size %d", len(bcryptHash), bcryptSize)
 	}
 
 	c, err := aes.NewCipher(key)
 	if err != nil {
-		return fmt.Errorf("Error while creating new AES cipher: %v", err)
+		return fmt.Errorf("securefile: Error while creating new AES cipher: %v", err)
 	}
 
 	gcm, err := cipher.NewGCMWithNonceSize(c, gcmNonceSize)
 	if err != nil {
-		return fmt.Errorf("Error while creating new GCM: %v", err)
+		return fmt.Errorf("securefile: Error while creating new GCM: %v", err)
 	}
 
 	if len(fulltext) < gcm.NonceSize() {
-		return fmt.Errorf("Plaintext is smaller than GCM nonce size")
+		return fmt.Errorf("securefile: Plaintext is smaller than GCM nonce size")
 	}
 
 	nonce, err := libpassword.Random(gcmNonceSize)
 	if err != nil {
-		return fmt.Errorf("Error while generating nonce for GCM: %v", err)
+		return fmt.Errorf("securefile: Error while generating nonce for GCM: %v", err)
 	}
 
 	ciphertext := gcm.Seal(nil, nonce, fulltext, nil)
@@ -147,7 +147,7 @@ func (rs *ReadSaver) Save(plaintext []byte) error {
 
 	err = ioutil.WriteFile(rs.Filepath, fileBytes, 0644)
 	if err != nil {
-		return fmt.Errorf("Error while writing to file: %v", err)
+		return fmt.Errorf("securefile: Error while writing to file: %v", err)
 	}
 
 	return nil
--- pkg/securefile/securefile_test.go
+++ pkg/securefile/securefile_test.go
@@ -7,9 +7,6 @@ import (
 
 const (
 	integrationTestEnvVar = "SECUREFILE_INTEGRATION_TEST"
-	testFilepath          = "./securefile.test"
-	testPassword          = "testpassword1234!@#$"
-	testPlaintext         = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
 )
 
 func TestMain(m *testing.M) {
@@ -21,68 +18,95 @@ func run(m *testing.M) int {
 	return m.Run()
 }
 
-func TestSaveWritesSecureFile(t *testing.T) {
-	fileInt := os.Getenv(integrationTestEnvVar)
-	if fileInt == "" {
-		t.Skipf("Set %s to run this test.", integrationTestEnvVar)
-	}
-
-	rs := &ReadSaver{
-		Filepath: testFilepath,
-		Password: testPassword,
-	}
-
-	err := rs.Save([]byte(testPlaintext))
-	if err != nil {
-		t.Fatalf("Want Save() err = nil, got: %v", err)
-	}
-	t.Cleanup(func() {
-		err := os.Remove(rs.Filepath)
-		if err != nil {
-			t.Fatalf("Want os.Remove() err = nil, got: %v", err)
-		}
-	})
-
-	_, err = os.Stat(rs.Filepath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			t.Fatalf("File `%s` does not exist", rs.Filepath)
-		}
-		t.Fatalf("Stat on file `%s` returned err = %v", rs.Filepath, err)
-	}
-
-}
-
 func TestReadReadsSecureFile(t *testing.T) {
 	fileInt := os.Getenv(integrationTestEnvVar)
 	if fileInt == "" {
 		t.Skipf("Set %s to run this test.", integrationTestEnvVar)
 	}
 
-	rs := &ReadSaver{
-		Filepath: testFilepath,
-		Password: testPassword,
+	tests := []struct {
+		name      string
+		filepath  string
+		password  string
+		plaintext string
+	}{
+		{"withPasswordAndPlaintext", "./securefile.test", "testpassword1234!@#$", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
+		{"withEmptyPlaintext", "./securefile.test", "testpassword1234!@#$", ""},
 	}
 
-	err := rs.Save([]byte(testPlaintext))
-	if err != nil {
-		t.Fatalf("Failed to save securefile: %v", err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rs := &ReadSaver{
+				Filepath: tt.filepath,
+				Password: tt.password,
+			}
+
+			err := rs.Save([]byte(tt.plaintext))
+			if err != nil {
+				t.Fatalf("Failed to save securefile: %v", err)
+			}
+			defer func() {
+				err := os.Remove(rs.Filepath)
+				if err != nil {
+					t.Fatalf("Want os.Remove() err = nil, got: %v", err)
+				}
+			}()
+
+			got, err := rs.Read()
+			if err != nil {
+				t.Fatalf("Want Read() err = nil, got: %v", err)
+			}
+
+			want := tt.plaintext
+
+			if string(got) != want {
+				t.Fatalf("Plaintext read from file: `%s`; want `%s`", string(got), want)
+			}
+		})
 	}
-	t.Cleanup(func() {
-		err := os.Remove(rs.Filepath)
-		if err != nil {
-			t.Fatalf("Want os.Remove() err = nil, got: %v", err)
-		}
-	})
+}
 
-	got, err := rs.Read()
-	if err != nil {
-		t.Fatalf("Want Read() err = nil, got: %v", err)
+func TestSaveWritesSecureFile(t *testing.T) {
+	fileInt := os.Getenv(integrationTestEnvVar)
+	if fileInt == "" {
+		t.Skipf("Set %s to run this test.", integrationTestEnvVar)
 	}
 
-	want := testPlaintext
+	tests := []struct {
+		name      string
+		filepath  string
+		password  string
+		plaintext string
+	}{
+		{"withPasswordAndPlaintext", "./securefile.test", "testpassword1234!@#$", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
+		{"withEmptyPlaintext", "./securefile.test", "testpassword1234!@#$", ""},
+	}
 
-	if string(got) != want {
-		t.Fatalf("Plaintext read from file: `%s`; want `%s`", string(got), want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rs := &ReadSaver{
+				Filepath: tt.filepath,
+				Password: tt.password,
+			}
+
+			err := rs.Save([]byte(tt.plaintext))
+			if err != nil {
+				t.Fatalf("Want Save() err = nil, got: %v", err)
+			}
+			defer func() {
+				err := os.Remove(rs.Filepath)
+				if err != nil {
+					t.Fatalf("Want os.Remove() err = nil, got: %v", err)
+				}
+			}()
+
+			_, err = os.Stat(rs.Filepath)
+			if err != nil {
+				if os.IsNotExist(err) {
+					t.Fatalf("File `%s` does not exist", rs.Filepath)
+				}
+				t.Fatalf("Stat on file `%s` returned err = %v", rs.Filepath, err)
+			}
+		})
 	}
 }
